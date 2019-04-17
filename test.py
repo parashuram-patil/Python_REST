6af3631dc0e50305d77d75f0af40f982a6e1ba36
parashuram-patil <parasharam_patil@ymail.com>
2019-04-17 13:51:31 +0530

    if __name__ == "__main__"
    if __name__ == "__main__"
     //Code block 1
    Else
     //Code block 2
    
    when we run file containing this code, block 1 executes and block 2 if imported i some other file

---
+++ test.py
@@ -0,0 +1,9 @@
+def my_function():
+    print("I am inside function")
+
+print("Always executed")
+
+if __name__ == "__main__":
+    print("Executed when invoked directly")
+else:
+    print("Executed when imported")
