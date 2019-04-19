f902cc1f6f568f22cd182762b7d55a54733056a2
parashuram-patil <parasharam_patil@ymail.com>
2019-04-19 16:43:23 +0530

    PUT Request Impl

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import render_template, Flask
+from flask import render_template, Flask, request
 
 app = Flask(__name__, template_folder="templates")
 
@@ -11,6 +11,12 @@ def home():
 @app.route('/print_name/<name>', methods=['GET'])
 def print_name(name):
     return 'Hello ' + name + '!!!'
+
+
+@app.route('/save_name', methods=['PUT'])
+def save_name():
+    body = request.get_json(silent=True)
+    return 'Hello ' + body["name"] + '!!!'
 
 
 if __name__ == '__main__':
