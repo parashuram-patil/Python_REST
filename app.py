30c72c5b1bd26ecaade52661c45dc60d9b96b94b
parashuram-patil <parasharam_patil@ymail.com>
2019-04-19 18:26:43 +0530

    Post API Impl - custom response code - custom header - mimetype - body 	{ 		"param" : "PSP", 		"header" : "PSP" 	} - Header 	Content-Type:application/json 	name:PSP

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import render_template, Flask, request, current_app
 
 app = Flask(__name__, template_folder="templates")
@@ -17,6 +19,35 @@ def print_name(name):
 def save_name():
     body = request.get_json(silent=True)
     response = 'Hello ' + body["name"] + '!!!'
+    return response
+
+
+@app.route('/validate_name/<name>', methods=['POST'])
+def validate_name(name):
+    body = request.get_json(silent=True)
+
+    status_code = 200
+    response_message = None
+    message = None
+
+    if not name == body["param"]:
+        status_code = 405
+        message = 'param not matching'
+    elif not request.headers.get('name') == body["header"]:
+        status_code = 406
+        message = 'header not matching'
+    else:
+        message = body
+
+    response_message = {
+        "response": message
+    }
+
+    response = current_app.response_class(
+        json.dumps(response_message),
+        status_code,
+        mimetype='application/json'
+    )
     return response
 
 
